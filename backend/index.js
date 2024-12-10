88eaa2600129db05015ef626d146cadf976c9c90
Shivaprasad-SK <shivaprasadk342@gmail.com>
2024-12-10 16:21:55 +0530

    .env file added

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const TodoModel = require("./Models/Todo");
 const cors = require("cors");
 
+const PORT = process.env.PORT;
+const mongoURL = process.env.MongoURL;
+
 // Define routes and middleware here
 // ...
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://ShivaprasadK:eWing1234@cluster0.ci9ghf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test"
-);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose
+  .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  });
 
 app.get("/get", async (req, res) => {
   try {
@@ -38,17 +46,17 @@ app.post("/add", async (req, res) => {
   }
 });
 
-app.delete('/delete/:id', async (req, res) => {
+app.delete("/delete/:id", async (req, res) => {
   try {
     const { id } = req.params;
     await TodoModel.findByIdAndDelete(id);
-    res.json({ message: 'Task deleted successfully' });
+    res.json({ message: "Task deleted successfully" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-app.put('/toggle/:id', async (req, res) => {
+app.put("/toggle/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const todo = await TodoModel.findById(id);
